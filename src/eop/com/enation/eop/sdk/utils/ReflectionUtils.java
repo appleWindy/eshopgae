385b8793f95a12f3acc20de109df5f94bc952a76
zhuyingjie8385@gmail.com <zhuyingjie8385@gmail.com@6dc02afa-e676-09df-a93a-47ffcb9bc308>
2013-03-31 03:25:03 +0000

    log出力を普通のlogにする

--- src/eop/com/enation/eop/sdk/utils/ReflectionUtils.java
+++ src/eop/com/enation/eop/sdk/utils/ReflectionUtils.java
@@ -16,10 +16,10 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.beanutils.PropertyUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 /**
@@ -31,7 +31,7 @@ import org.springframework.util.Assert;
  */
 public class ReflectionUtils {
 
-	private static Logger logger = LoggerFactory.getLogger(ReflectionUtils.class);
+	private static Logger logger = Logger.getLogger(ReflectionUtils.class.getName());
 
 	/**
 	 * 直接读取对象属性值, 无视private/protected修饰符, 不经过getter函数.
@@ -48,7 +48,7 @@ public class ReflectionUtils {
 		try {
 			result = field.get(object);
 		} catch (IllegalAccessException e) {
-			logger.error("不可能抛出的异常{}", e.getMessage());
+			logger.log(Level.SEVERE,"不可能抛出的异常{}", e.getMessage());
 		}
 		return result;
 	}
@@ -67,7 +67,7 @@ public class ReflectionUtils {
 		try {
 			field.set(object, value);
 		} catch (IllegalAccessException e) {
-			logger.error("不可能抛出的异常:{}", e.getMessage());
+			logger.log(Level.SEVERE, "不可能抛出的异常:{}", e.getMessage());
 		}
 	}
 
@@ -166,19 +166,19 @@ public class ReflectionUtils {
 		Type genType = clazz.getGenericSuperclass();
 	 
 		if (!(genType instanceof ParameterizedType)) {
-			logger.warn(clazz.getSimpleName() + "'s superclass not ParameterizedType");
+			logger.warning(clazz.getSimpleName() + "'s superclass not ParameterizedType");
 			return Object.class;
 		}
 
 		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
 
 		if (index >= params.length || index < 0) {
-			logger.warn("Index: " + index + ", Size of " + clazz.getSimpleName() + "'s Parameterized Type: "
+			logger.warning("Index: " + index + ", Size of " + clazz.getSimpleName() + "'s Parameterized Type: "
 					+ params.length);
 			return Object.class;
 		}
 		if (!(params[index] instanceof Class)) {
-			logger.warn(clazz.getSimpleName() + " not set the actual class on superclass generic parameter");
+			logger.warning(clazz.getSimpleName() + " not set the actual class on superclass generic parameter");
 			return Object.class;
 		}
 
@@ -190,25 +190,25 @@ public class ReflectionUtils {
 
 		Type[] genTypes = clazz.getGenericInterfaces();
 		if(genTypes==null || genTypes.length==0 ){
-			logger.warn(clazz.getSimpleName() + "'s not impl interface ");
+			logger.warning(clazz.getSimpleName() + "'s not impl interface ");
 			return Object.class;
 		}
 		
 		
 		if (!(genTypes[0] instanceof ParameterizedType)) {
-			logger.warn(clazz.getSimpleName() + "'s superclass not ParameterizedType");
+			logger.warning(clazz.getSimpleName() + "'s superclass not ParameterizedType");
 			return Object.class;
 		}
 
 		Type[] params = ((ParameterizedType) genTypes[0]).getActualTypeArguments();
 	
 		if (index >= params.length || index < 0) {
-			logger.warn("Index: " + index + ", Size of " + clazz.getSimpleName() + "'s Parameterized Type: "
+			logger.warning("Index: " + index + ", Size of " + clazz.getSimpleName() + "'s Parameterized Type: "
 					+ params.length);
 			return Object.class;
 		}
 		if (!(params[index] instanceof Class)) {
-			logger.warn(clazz.getSimpleName() + " not set the actual class on superclass generic parameter");
+			logger.warning(clazz.getSimpleName() + " not set the actual class on superclass generic parameter");
 			return Object.class;
 		}
 
